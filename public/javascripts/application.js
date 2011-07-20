09891e9cfedaaf1fbe03dd6a1b936319c544a2f2
Ryan Atallah <ryan.atallah@mac.com>
2011-07-20 10:45:47 -0600

    IE Compatibility Fixes pt.1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -100,3 +100,4 @@ function SelectText(element) {
 $(function document() {
   $("nav.secondary").sap();
 });
+
---
+++ public/javascripts/ie.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  alert("blah");
+  $("table tr:even").css("background-color", "#ebebeb");
+});
