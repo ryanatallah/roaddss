a952d029ebf8775fb21210158d3bd31fd57548ea
Ryan Atallah <ryan.atallah@mac.com>
2011-08-20 13:01:08 -0600

    fixed reload with hash

--- public/javascripts/jquery.rest_in_place.js
+++ public/javascripts/jquery.rest_in_place.js
@@ -152,8 +152,7 @@ RestInPlaceEditor.prototype = {
     if ($.browser.msie && $.browser.version.substr(0,1)<9) {
       var element_container = this.element.parent().parent().parent().parent();
       var tag = element_container.prev("h4").attr("id");
-      window.location.hash = tag;
-      window.location.reload();
+      window.location = window.location.pathname + "#" + tag;
     } else {
 
       if (this.no_format == false) {
